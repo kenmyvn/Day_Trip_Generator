dd9b92704046248df88f42e62a7048540d8b2e51
Kendall Morgan <kuyukip@gmail.com>
2022-04-09 14:01:41 -0500

    Updated functions to be single responsibility functions I think?

--- main.py
+++ main.py
@@ -20,54 +20,60 @@ entertainments = ['visiting a temple', 'going to a cat cafe', 'going to a game c
 restaurants = ['MOS Burger', 'Inari Steak', 'Coco Curry Ichibanya', 'Hamazushi', '7-11']
 
 import random
+final_destination = 'n'
 
-def select_random_destination():
-    random_destination_input = 'n'
+def select_random_destination(final_destination):
     print('Welcome to Japan! If you are not sure what you want to do for vacation, no worries.')
     destination_max = len(destinations) - 1
-    while random_destination_input == 'n':
+    while final_destination == 'n':
         destination_index = random.randint(0, destination_max)
         destination = destinations[destination_index]
-        random_destination_input = input(f'We have selected {destination} for your destination. Does this sound good? Enter y/n: ')
+        final_destination = input(f'We have selected {destination} for your destination. Does this sound good? Enter y/n: ')
     print('Great, glad that is decided. Moving on.')
 
-select_random_destination()
+select_random_destination(final_destination)
 
 print('')
+final_transportation = 'n'
 
-def select_random_transportation():
-    random_transportation_input = 'n'
+def select_random_transportation(final_transportation):
     transportation_max = len(transportations) - 1
-    while random_transportation_input == 'n':
+    while final_transportation == 'n':
         transportation_index = random.randint(0, transportation_max)
         transportation = transportations[transportation_index]
-        random_transportation_input = input(f'We have selected {transportation} as your mode of transportation. Does this sound good? Enter y/n: ')
+        final_transportation = input(f'We have selected {transportation} as your mode of transportation. Does this sound good? Enter y/n: ')
     print('Great, glad that is decided. Moving on.')
     
-select_random_transportation()
+select_random_transportation(final_transportation)
 
 print('')
+final_entertainment = 'n'
 
-def select_random_entertainment():
-    random_entertainment_input = 'n'
+def select_random_entertainment(final_entertainment):
     entertainment_max = len(entertainments) - 1
-    while random_entertainment_input == 'n':
+    while final_entertainment == 'n':
         entertainment_index = random.randint(0, entertainment_max)
         entertainment = entertainments[entertainment_index]
-        random_entertainment_input = input(f'We have selected {entertainment} for you to do this afternoon. Does this sound good? Enter y/n: ')
+        final_entertainment = input(f'We have selected {entertainment} for you to do this afternoon. Does this sound good? Enter y/n: ')
     print('Great, that is a lovely choice. Moving on.')
 
-select_random_entertainment()
+select_random_entertainment(final_entertainment)
 
 print('')
+final_restaurant = 'n'
 
-def select_random_restaurant():
-    random_restaurant_input = 'n'
+def select_random_restaurant(final_restaurant):
     restaurant_max = len(restaurants) - 1
-    while random_restaurant_input == 'n':
+    while final_restaurant == 'n':
         restaurant_index = random.randint(0, restaurant_max)
         restaurant = restaurants[restaurant_index]
-        random_restaurant_input = input(f'We have selected {restaurant} for as your dining option. Does this sound good? Enter y/n: ')
+        final_restaurant = input(f'We have selected {restaurant} for as your dining option. Does this sound good? Enter y/n: ')
     print('Great, that is some yummy food. Moving on.')
 
-select_random_restaurant()
+select_random_restaurant(final_restaurant)
+
+print('')
+print('Congrats! Your random Japan trip has been decided. Please just make sure that this is what you wanted.')
+print('Your trip looks like: ')
+
+# def create_trip():
