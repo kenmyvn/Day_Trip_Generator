6d936619f5255609b19fed11a766b13187933794
Kendall Morgan <kuyukip@gmail.com>
2022-04-09 13:44:49 -0500

    Created functions for all of the lists to create a day trip

--- main.py
+++ main.py
@@ -13,13 +13,13 @@
 # (10 points): As a user, I want to display my completed trip in the console.
 # (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
 
-import random
 
 destinations = ['Tokyo', 'Osaka', 'Kyoto', 'Sendai', 'Gunma', 'Hokkaido']
 transportations = ['high-speed train', 'local train', 'plane', 'car', 'Mario Go-Kart']
 entertainments = ['visiting a temple', 'going to a cat cafe', 'going to a game center', 'participating in a tea ceremony', 'climbing a mountain']
 restaurants = ['MOS Burger', 'Inari Steak', 'Coco Curry Ichibanya', 'Hamazushi', '7-11']
 
+import random
 
 def select_random_destination():
     random_destination_input = 'n'
@@ -30,5 +30,44 @@ def select_random_destination():
         destination = destinations[destination_index]
         random_destination_input = input(f'We have selected {destination} for your destination. Does this sound good? Enter y/n: ')
     print('Great, glad that is decided. Moving on.')
-    
+
 select_random_destination()
+
+print('')
+
+def select_random_transportation():
+    random_transportation_input = 'n'
+    transportation_max = len(transportations) - 1
+    while random_transportation_input == 'n':
+        transportation_index = random.randint(0, transportation_max)
+        transportation = transportations[transportation_index]
+        random_transportation_input = input(f'We have selected {transportation} as your mode of transportation. Does this sound good? Enter y/n: ')
+    print('Great, glad that is decided. Moving on.')
+    
+select_random_transportation()
+
+print('')
+
+def select_random_entertainment():
+    random_entertainment_input = 'n'
+    entertainment_max = len(entertainments) - 1
+    while random_entertainment_input == 'n':
+        entertainment_index = random.randint(0, entertainment_max)
+        entertainment = entertainments[entertainment_index]
+        random_entertainment_input = input(f'We have selected {entertainment} for you to do this afternoon. Does this sound good? Enter y/n: ')
+    print('Great, that is a lovely choice. Moving on.')
+
+select_random_entertainment()
+
+print('')
+
+def select_random_restaurant():
+    random_restaurant_input = 'n'
+    restaurant_max = len(restaurants) - 1
+    while random_restaurant_input == 'n':
+        restaurant_index = random.randint(0, restaurant_max)
+        restaurant = restaurants[restaurant_index]
+        random_restaurant_input = input(f'We have selected {restaurant} for as your dining option. Does this sound good? Enter y/n: ')
+    print('Great, that is some yummy food. Moving on.')
+
+select_random_restaurant()
