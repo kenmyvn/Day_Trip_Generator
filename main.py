9f611e17e6d68dce5171026bdf2d238a359ac873
Kendall Morgan <kuyukip@gmail.com>
2022-04-09 13:12:24 -0500

    Created first function to run will continue utilizing this for following functions

--- main.py
+++ main.py
@@ -13,8 +13,22 @@
 # (10 points): As a user, I want to display my completed trip in the console.
 # (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
 
+import random
 
 destinations = ['Tokyo', 'Osaka', 'Kyoto', 'Sendai', 'Gunma', 'Hokkaido']
 transportations = ['high-speed train', 'local train', 'plane', 'car', 'Mario Go-Kart']
 entertainments = ['visiting a temple', 'going to a cat cafe', 'going to a game center', 'participating in a tea ceremony', 'climbing a mountain']
 restaurants = ['MOS Burger', 'Inari Steak', 'Coco Curry Ichibanya', 'Hamazushi', '7-11']
+
+
+def select_random_destination():
+    random_destination_input = 'n'
+    print('Welcome to Japan! If you are not sure what you want to do for vacation, no worries.')
+    destination_max = len(destinations) - 1
+    while random_destination_input == 'n':
+        destination_index = random.randint(0, destination_max)
+        destination = destinations[destination_index]
+        random_destination_input = input(f'We have selected {destination} for your destination. Does this sound good? Enter y/n: ')
+    print('Great, glad that is decided. Moving on.')
+    
+select_random_destination()
